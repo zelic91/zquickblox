aca0de5be03b015e8be75d5a4639479e7d0f8bdc
Thuong Nguyen <thuongnh.uit@gmail.com>
2016-06-25 13:42:15 +0700

    Fix config in generator

--- lib/generators/templates/zquickblox.rb
+++ lib/generators/templates/zquickblox.rb
@@ -1,5 +1,5 @@
 ZQuickblox.configure do |config|
-  config.app_id      = ENV['QB_APP_ID'],
-  config.auth_key    = ENV['QB_AUTH_KEY'],
+  config.app_id      = ENV['QB_APP_ID']
+  config.auth_key    = ENV['QB_AUTH_KEY']
   config.auth_secret = ENV['QB_AUTH_SECRET']
 end
