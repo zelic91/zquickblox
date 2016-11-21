ca3be41b5da26c807bbcc81a21d74be6161dfe51
Thuong Nguyen <thuongnh.uit@gmail.com>
2016-11-21 08:15:01 +0700

    Fix test fixtures.

--- test/lib/zquickblox/user_test.rb
+++ test/lib/zquickblox/user_test.rb
@@ -3,12 +3,12 @@ require_relative '../../test_helper'
 describe ZQuickblox::User do
 
   it "should create new user" do
-    user = ZQuickblox::User.create({email: "#{Time.now.to_i}@email.com", password: "#{Time.now.to_i}"})
+    user = ZQuickblox::User.create({email: "#{rand(99..Time.now.to_i)}@email.com", password: "#{Time.now.to_i}"})
     user.id.wont_be_nil
   end
 
   it "should find user" do
-    user = ZQuickblox::User.find("#{Time.now.to_i}@email.com")
+    user = ZQuickblox::User.find("#{rand(99..Time.now.to_i)}@email.com")
     assert user.nil?
   end
 end
